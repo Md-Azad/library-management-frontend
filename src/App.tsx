65940790d24c2e9055727c2325f6560f8a2577d3
Md Azad Hossain <sokalazad@gmail.com>
2025-07-02 04:10:51 +0200

    library-management-frontend project initialized with vite and tailwind, daisyui and router added

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">Library management system</h1>
+      <button className="btn btn-xs">Xsmall</button>
+      <button className="btn btn-sm">Small</button>
+      <button className="btn">Medium</button>
+      <button className="btn btn-lg">Large</button>
+      <button className="btn btn-xl">Xlarge</button>
+    </>
+  );
+}
+
+export default App;
